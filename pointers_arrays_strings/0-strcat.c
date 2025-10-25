19b09ffc84a0093e1db3eeb26730fbb1fbc375dd
AbdullahAldaghaym <Abdullah97sd@gmail.com>
2025-10-25 20:23:09 +0000

    Initial commit - Add 0-strcat function

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string to append
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+    char *ptr = dest;
+
+    /* Move to the end of dest */
+    while (*ptr != '\0')
+        ptr++;
+
+    /* Copy src to end of dest */
+    while (*src != '\0')
+    {
+        *ptr = *src;
+        ptr++;
+        src++;
+    }
+
+    /* Add null terminator */
+    *ptr = '\0';
+
+    return (dest);
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+# C - Pointers, Arrays and Strings
+
+This project contains C programs that demonstrate various operations with pointers, arrays, and strings. All functions are implemented without using the standard library functions.
+
+## Project Structure
