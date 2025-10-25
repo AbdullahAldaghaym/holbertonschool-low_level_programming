0925e4d3ee8d175dea151f385b8bc41fb096f16b
AbdullahAldaghaym <Abdullah97sd@gmail.com>
2025-10-25 22:05:54 +0000

    Add print_buffer function

---
+++ pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ */
+void print_buffer(char *b, int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	for (i = 0; i < size; i += 10)
+	{
+		/* Print position */
+		printf("%08x: ", i);
+
+		/* Print hex bytes */
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+				printf("%02x", (unsigned char)b[i + j]);
+			else
+				printf("  ");
+
+			if (j % 2)
+				printf(" ");
+		}
+
+		/* Print ASCII characters */
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j >= size)
+				break;
+
+			if (isprint(b[i + j]))
+				printf("%c", b[i + j]);
+			else
+				printf(".");
+		}
+
+		printf("\n");
+	}
+}
