fbfbde5b6f930160248f309d5ce7db0f2bd110f5
AbdullahAldaghaym <Abdullah97sd@gmail.com>
2025-10-26 00:50:18 +0000

    Completely fix Fibonacci program - proper commas and large number handling 1

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,39 +1,47 @@
 #include <stdio.h>
+
+/**
+ * main - prints first 98 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	int i = 3;
-	unsigned long a = 1, b = 2, c;
-	unsigned long a1, a2, b1, b2, c1, c2;
+	int i;
+	unsigned long a = 1, b = 2, next;
+	unsigned long a_h, a_l, b_h, b_l, n_h, n_l;
+
 	printf("%lu, %lu", a, b);
-	while (i <= 92)
+
+	for (i = 3; i <= 92; i++)
 	{
-		c = a + b;
-		printf(", %lu", c);
+		next = a + b;
+		printf(", %lu", next);
 		a = b;
-		b = c;
-		i++;
+		b = next;
 	}
-	a1 = a / 10000000000;
-	a2 = a % 10000000000;
-	b1 = b / 10000000000;
-	b2 = b % 10000000000;
-	while (i <= 98)
+
+	a_h = a / 10000000000;
+	a_l = a % 10000000000;
+	b_h = b / 10000000000;
+	b_l = b % 10000000000;
+
+	for (; i <= 98; i++)
 	{
-		c1 = a1 + b1;
-		c2 = a2 + b2;
-		if (c2 > 9999999999)
-		{
-			c1 += 1;
-			c2 %= 10000000000;
-		}
+		n_l = a_l + b_l;
+		n_h = a_h + b_h + (n_l / 10000000000);
+		n_l %= 10000000000;
+
 		printf(", ");
-		if (c1) printf("%lu%010lu", c1, c2);
-		else printf("%lu", c2);
-		a1 = b1;
-		a2 = b2;
-		b1 = c1;
-		b2 = c2;
-		i++;
+		if (n_h)
+			printf("%lu%010lu", n_h, n_l);
+		else
+			printf("%lu", n_l);
+
+		a_h = b_h;
+		a_l = b_l;
+		b_h = n_h;
+		b_l = n_l;
 	}
 	printf("\n");
 	return (0);
