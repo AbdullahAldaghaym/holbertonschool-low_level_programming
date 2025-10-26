f74bd8c5239b5ce861799436b369fc8e568c78ef
AbdullahAldaghaym <Abdullah97sd@gmail.com>
2025-10-26 00:49:01 +0000

    Completely fix Fibonacci program - proper commas and large number handling 1

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
-
-/**
- * main - prints first 98 Fibonacci numbers
- *
- * Return: Always 0
- */
 int main(void)
 {
 	int i = 3;
 	unsigned long a = 1, b = 2, c;
 	unsigned long a1, a2, b1, b2, c1, c2;
-
 	printf("%lu, %lu", a, b);
-
 	while (i <= 92)
 	{
 		c = a + b;
@@ -21,28 +13,22 @@ int main(void)
 		b = c;
 		i++;
 	}
-
 	a1 = a / 10000000000;
 	a2 = a % 10000000000;
 	b1 = b / 10000000000;
 	b2 = b % 10000000000;
-
 	while (i <= 98)
 	{
 		c1 = a1 + b1;
 		c2 = a2 + b2;
 		if (c2 > 9999999999)
 		{
-			c1 += c2 / 10000000000;
+			c1 += 1;
 			c2 %= 10000000000;
 		}
-
 		printf(", ");
-		if (c1)
-			printf("%lu%010lu", c1, c2);
-		else
-			printf("%lu", c2);
-
+		if (c1) printf("%lu%010lu", c1, c2);
+		else printf("%lu", c2);
 		a1 = b1;
 		a2 = b2;
 		b1 = c1;
