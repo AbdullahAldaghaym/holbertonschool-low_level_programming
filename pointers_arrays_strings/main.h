729e122e4f966f0351074e9d89a8f9e1dafeace2
AbdullahAldaghaym <Abdullah97sd@gmail.com>
2025-11-01 11:20:39 +0000

    Add _memset function

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -25,4 +25,5 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char *_strcpy(char *dest, char *src);
+char *_memset(char *s, char b, unsigned int n);
 #endif /* MAIN_H */
